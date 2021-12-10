028883e1ed38d8516808ecf7c403d511dece6bc2
webdevandseo <webdevandseoua@gmail.com>
2021-12-10 16:28:51 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,8 @@ sudo apt update
 sudo apt install wget -y
 git clone https://github.com/elejke/docker-socks5.git
 cd docker-socks5
-nano Dockerfile
+sed -i -e 's/admin2/admin/g' Dockerfile
+sed -i -e 's/MyPassword/159357/g' Dockerfile
 echo "-----------------------------------------------------------------------------"
 echo "Тепер просто в проксі вписуєте свій IP (SOCKS5) і порт 80"
 echo "SMART TRADE"
