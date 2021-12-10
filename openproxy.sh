90c1d95c469cf324b9af60580cc6c57f580d932a
webdevandseo <webdevandseoua@gmail.com>
2021-12-10 16:36:44 +0200

    Update openproxy.sh

--- openproxy.sh
+++ openproxy.sh
@@ -12,11 +12,9 @@ rm -f get-docker.sh
 sudo usermod -aG docker $USER
 git clone https://github.com/elejke/docker-socks5.git
 cd docker-socks5
-sed -i -e 's/john/admin/g' Dockerfile
-sed -i -e 's/MyPassword/159357/g' Dockerfile
 sudo docker build -t socks5 .
-sudo sudo docker run -d -p 80:1080 -v ${PWD}/sockd.conf:/etc/sockd.conf socks5
+sudo docker run -d -p 80:1080 socks5
 echo "-----------------------------------------------------------------------------"
 echo "Тепер просто в проксі вписуєте свій IP (SOCKS5) і порт 80"
-echo "SMART TRADE" s
+echo "SMART TRADE"
 echo "-----------------------------------------------------------------------------"
